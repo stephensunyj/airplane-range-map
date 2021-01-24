0c8c37603fc7316c7d09b350580f9ebc15fa7ebf
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2021-01-24 00:23:43 -0500

    final updates

---
+++ dbConn.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$db = mysqli_connect("webhosting2011.is.cc:3306","joshuane_joshneronha","joshuajn8","joshuane_airlines");
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
